1a2c0b47832967086cb9cc5e79cef08ef3cccc38
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-17 12:21:19 -0300

    tentando

--- bank/bank.js
+++ bank/bank.js
@@ -52,15 +52,12 @@ class Conta{
 
 class Poupanca extends Conta{
     constructor(numero, cpf, saldo, ativa, diaAniversarioPoupanca){
-        super(numero),
-        super(cpf),
-        super(saldo),
-        super(ativa),
+        super(numero,cpf,saldo,ativa),
         this.diaAniversarioPoupanca = diaAniversarioPoupanca
     }
     correcao(dia){
         if(dia == this.diaAniversarioPoupanca){
-            super().credito(super().saldo*0.05)
+            this.credito(this.saldo*0.05)
         }
         else{
             console.log("O dia não é o mesmo da conta...")
@@ -73,10 +70,12 @@ class Poupanca extends Conta{
 const leia = require("prompt-sync")()
 let op = ""
 let valor = 0
-let mensagemInicio = `[Nome Do Banco]`
 
+console.log("Flor Do Din Din")
+console.log("Banco que rouba mais do que o da arminha e o do nove dedos :D \n")
 console.log("1 - Conta Poupança")
-console.log("2 - Conta corrente")
+console.log("2 - Conta corrente ")
+console.log("3 - Sair \n")
 tipo = leia("Digite o numero do tipo de conta a ser aberta: ")
 
 if (tipo=="1"){
@@ -84,26 +83,32 @@ if (tipo=="1"){
 
     let numero = parseInt(leia("Digite o numero da conta: "))
     let cpf = leia("Digite o cpf: ")
-    
-    let c1 = new Poupanca(numero,cpf,0,false, 1)
-    c1.ativar()
+    let diaAniversarioPoupanca = leia("Digite o dia de criação da conta: ")
+    let cp = new Poupanca(numero,cpf,0,false, diaAniversarioPoupanca)
+    cp.ativar()
 
     for(let x=1; x<=10; x++){
-        console.log("Movimento "+x)
-        console.log("Saldo da conta R$: "+c1.saldo)
+        console.log("\n \n Movimento "+x+"\n")
+        console.log("Saldo da conta R$: "+cp.saldo)
         valor = parseInt(leia("Digite o valor: "))
         op = leia("Digite D - débito ou C - crédito: ").toUpperCase()
         if(op=="C"){
-            c1.credito(valor)
+            cp.credito(valor)
         }
         else if(op=="D"){
-            c1.debito(valor)
+            cp.debito(valor)
         }
     }
-
+    console.log("\n \n Saldo final da conta R$: "+cp.saldo)
     let dia = leia("Digite qual é o dia hojé: ")
-    c1.correcao(dia)
-    
+    cp.correcao(dia)
+    console.log("\n Saldo final da conta R$: "+cp.saldo)
+}
 
-    console.log("Saldo final da conta R$: "+c1.saldo)
+else if(tipo=="2"){
+
+}
+
+if else(tipo=="3"){
+    console.log("Você escolheu encerrar o atendimento.. Tchau!!")
 }
