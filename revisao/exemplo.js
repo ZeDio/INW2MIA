c68cce0ebec5761bf7d6f87988f7c024cb533e21
Logon Aluno <logonrmlocal@fiap.com.br>
2023-06-06 08:43:23 -0300

    So pra ter o commit

---
+++ revisao/exemplo.js
@@ -0,0 +1,31 @@
+//npm install prompt-sync
+
+// console.log("oi \n \n mundo")
+
+// console.log("NOME\tNOTA")
+// console.log("Eduardo\t5,5")
+// console.log("Maria\t10")
+
+// let nome = "José Diogo"
+// let valor1 =0
+// let valor2 =0
+// console.log(valor1+valor2)
+// console.log(nome)
+// console.log("Oi "+nome+" o valor 1 é "+valor1)
+// console.log("A soma dos dois valores é "+(valor1+valor2))
+
+// const leia = require("prompt-sync")()
+// let valor1 = 0
+//let valor2 = 0
+// let nome = leia("Digite o seu nome: ")
+// valor1 = parseInt(leia("Digite o valor 1: "))
+// valor2 = parseInt(leia("Digite o valor 2: "))
+// console.log("Oi "+nome+". A soma é igual "+(valor1+valor2))
+
+//Desafio
+
+const leia = require("prompt-sync")()
+let nome = leia("Digite o seu nome: ")
+let graus = parseInt(leia("Digite quantos graus está fazendo agora.. : "))
+let Kelvin = (273+graus)
+console.log("Olá "+nome+" agora está fazedo "+Kelvin+"°k..")
