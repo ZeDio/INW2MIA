cd32b47f6b84189ec22dc5a9d79ef0b11ce5392f
ZeDio <jose.diogo100407@gmail.com>
2023-07-12 08:45:07 -0300

    Primeira Parte Do Portifolio Pronta Portifolio

--- Portifolio/Js/Efeito.js
+++ Portifolio/Js/Efeito.js
@@ -3,3 +3,4 @@ window.sr = ScrollReveal({ reset:true});
 sr.reveal('.Mi', {duration: 1000});
 sr.reveal('.Mi2', {duration: 1000});
 sr.reveal('.header', {duration: 1000});
+sr.reveal('.metade', {duration: 1000});
