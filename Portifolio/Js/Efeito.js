69453b724a072bb4dc93d0dac2399455452a6b8b
ZeDio <jose.diogo100407@gmail.com>
2023-07-14 19:22:44 -0300

    Portifolio Apresentavel

--- Portifolio/Js/Efeito.js
+++ Portifolio/Js/Efeito.js
@@ -2,5 +2,3 @@ window.sr = ScrollReveal({ reset:true});
 
 sr.reveal('.Mi', {duration: 1000});
 sr.reveal('.Mi2', {duration: 1000});
-sr.reveal('.header', {duration: 1000});
-sr.reveal('.metade', {duration: 1000});
